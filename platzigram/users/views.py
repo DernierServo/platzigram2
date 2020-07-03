a0e80915b6e6b648c190b330aa3fefefdbcb5252
dservo <arrasco.cs@gmail.com>
2020-07-03 12:07:07 -0500

    Validation our Signup form

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -5,15 +5,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-# Exception
-from django.db.utils import IntegrityError
-
-# Models
-from django.contrib.auth.models import User
-from users.models import Profile
-
 # Forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 
 def login_view(request):
@@ -44,42 +37,19 @@ def login_view(request):
 def signup(request):
     """Sign up view."""
     if request.method == 'POST':
-        v_username = request.POST['username']
-        v_passwd = request.POST['passwd']
-        v_passwd_confirmation = request.POST['passwd_confirmation']
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('n_users-login')
+    else:
+        form = SignupForm()
 
-        if v_passwd != v_passwd_confirmation:
-            return render(
-                request,
-                'users/signup.html',
-                {
-                    'error': 'Password confirmation does not match!'
-                }
-            )
-        try:
-            user = User.objects.create_user(username=v_username, password=v_passwd)
-        except IntegrityError:
-            return render(
-                request,
-                'users/signup.html',
-                {
-                    'error': 'Username is already in use!'
-                }
-            )                  
-                
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-
-        profile = Profile(user=user)
-        profile.save()
-
-        return redirect('n_users-login')
-        
     return render(
-        request, 
-        'users/signup.html'
+        request = request,
+        template_name = 'users/signup.html',
+        context = {
+            'form':form
+        }
     )
 
 
